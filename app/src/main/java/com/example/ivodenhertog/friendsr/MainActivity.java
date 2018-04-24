e6b9dd0a5bd2209332b2149c6f1f4dd3a604606b
Ivo den Hertog <ivodenhertog@gmail.com>
2018-04-24 12:29:17 +0200

    Building base code of the program.

--- app/src/main/java/com/example/ivodenhertog/friendsr/MainActivity.java
+++ app/src/main/java/com/example/ivodenhertog/friendsr/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.ivodenhertog.friendsr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Friend> friends = new ArrayList<>();
+    private Friend friend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 10; i++) {
+            friends.add(addNewFriend(i));
+        }
+    }
+
+    public Friend addNewFriend(int i) {
+        String[] names = getResources().getStringArray(R.array.names);
+
+        String name = names[i];
+        String bio = "This is the bio of " + name;
+        String nameLower = name.toLowerCase();
+
+        int id = getResources().getIdentifier(
+                "" +  nameLower, "drawable", getPackageName());
+
+        return new Friend(name, bio, id);
     }
 }
